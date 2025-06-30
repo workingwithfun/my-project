63a990016d59d91ed2958677c09d8b470ceb0abf
workingwithfun <ansariadi65@gmail.com>
2025-06-30 12:45:34 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,8 +14,7 @@ import os
 from pathlib import Path
 from decouple import config
 import environ
-import pymysql
-pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
