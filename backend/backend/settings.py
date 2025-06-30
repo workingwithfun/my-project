07663cbd38bc99d353f112f0eaa2053cabc305fc
workingwithfun <ansariadi65@gmail.com>
2025-06-30 13:14:10 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-rye%#j258hjmjoqnk3eu9**01ep9*^2&cu8o8&fl+bnxx%nz_m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['https://my-project-1-gzb7.onrender.com/', 'localhost']
+ALLOWED_HOSTS = ['my-project-1-gzb7.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
