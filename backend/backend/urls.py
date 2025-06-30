ca2d64a8e3cdeab817e2359d1227288fccfc150d
workingwithfun <ansariadi65@gmail.com>
2025-06-30 12:59:51 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
 from django.urls import include, path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,     # For login
@@ -27,6 +29,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authapp.urls')),
+    path("", lambda request: JsonResponse({"message": "Django API is running!"})),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
